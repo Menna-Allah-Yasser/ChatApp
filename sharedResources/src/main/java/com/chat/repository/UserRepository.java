4442e0467e4dbefeb7648e138dff36f4fe5814aa
sama-kamel2462 <sama.mohamed2462@gmail.com>
2025-02-04 14:32:26 +0200

    login and sign-up

--- sharedResources/src/main/java/com/chat/repository/UserRepository.java
+++ sharedResources/src/main/java/com/chat/repository/UserRepository.java
@@ -1,4 +1,28 @@
 package com.chat.repository;
 
-public interface UserRepository {
+
+import com.chat.entity.User;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface UserRepository  extends Remote {
+
+
+    public User findUserByPhoneNumber(String phoneNumber)   throws RemoteException;
+
+    public void deleteUserByPhoneNumber(String phoneNumber) throws RemoteException;
+
+    public  void UpdateUser(User userDTO ) throws  RemoteException;
+
+    public  void addNewUser(User userDTO) throws RemoteException;
+
+    public void updateStatus(int  userId ,String status) throws RemoteException ;
+
+    public void updateChatbotEnabled(int  userId ,Boolean status) throws RemoteException;
+
+    public  boolean authenticateUser(String phoneNumber, String password) throws RemoteException;
+
+    public void updateOnline(int userId ,Boolean isOnline);
+
 }
