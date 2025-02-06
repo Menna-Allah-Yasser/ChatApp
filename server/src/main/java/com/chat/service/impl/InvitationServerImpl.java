18e0f5d9b685749d05c56e426d20a5c0cc94120a
AhmedAbdElAleem01 <ahmadmu.abdelaleem@gmail.com>
2025-02-06 20:56:44 +0200

    invitationService implementation

---
+++ server/src/main/java/com/chat/service/impl/InvitationServerImpl.java
@@ -0,0 +1,54 @@
+package com.chat.service.impl;
+
+import com.chat.dao.impl.InvitationService;
+import com.chat.entity.InvStatus;
+import com.chat.entity.Invitation;
+import com.chat.entity.User;
+import com.chat.service.repository.InvitationServerRepository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InvitationServerImpl implements InvitationServerRepository {
+    InvitationService invitationService;
+
+    @Override
+    public List<Invitation> getUserFriendRequests(int userID) {
+        return invitationService.getAllInvitationsByStatus(userID, InvStatus.WAIT);
+    }
+
+    @Override
+    public List<Integer> getUserFriends(int userID) {
+        return invitationService.getFriends(userID);
+    }
+
+    @Override
+    public boolean updateFriendsRequestStatus(Invitation request) {
+        return invitationService.updateStatus(request);
+    }
+
+    @Override
+    public boolean sendFriendRequest(int senderId, List<User> users) {
+        List<Invitation> requests = new ArrayList<>();
+        List<Invitation> receivedInvitations = invitationService.getAllInvitationsByStatus(senderId, InvStatus.WAIT);
+
+        Map<Integer, Invitation> invitationMap = new HashMap<>();
+        for (Invitation invitation : receivedInvitations) {
+            invitationMap.put(invitation.getSenderId(), invitation);
+        }
+
+        for (User user : users) {
+            Invitation existingInvitation = invitationMap.get(user.getUserId());
+            if (existingInvitation != null) {
+                existingInvitation.setStatus(InvStatus.ACCEPT);
+                invitationService.updateStatus(existingInvitation);
+            } else {
+                requests.add(new Invitation(senderId, user.getUserId(), InvStatus.WAIT));
+            }
+        }
+        return invitationService.addListOfInvitation(requests);
+    }
+
+}
