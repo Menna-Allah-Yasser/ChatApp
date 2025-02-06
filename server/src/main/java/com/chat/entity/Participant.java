c8f2b6e3b339cdc48a46604f20dce86d21a9e81a
khaled1150 <khaledhero550@yahoo.com>
2025-02-06 19:59:06 +0200

    pushing

--- server/src/main/java/com/chat/entity/Participant.java
+++ server/src/main/java/com/chat/entity/Participant.java
@@ -1,4 +1,53 @@
 package com.chat.entity;
 
-public class Participant {
+import java.io.Serializable;
+
+public class Participant implements Serializable {
+    private int chatId;
+    private int particpantId;
+    private State state;
+    private Category category;
+
+    public Participant(int chatId, int particpantId, State state, Category category) {
+        this.chatId = chatId;
+        this.particpantId = particpantId;
+        this.state = state;
+        this.category = category;
+    }
+
+    public Participant() {
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getChatId() {
+        return chatId;
+    }
+
+    public int getParticpantId() {
+        return particpantId;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public enum Category {
+        WORK, FAMILY, FRIEND;
+    }
+
+    public enum State {
+        BLOCKED, AVAILABLE;
+    }
+
 }
---
+++ server/src/main/java/com/chat/service/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.chat.service.repository;
+
+import com.chat.entity.Notification;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public interface NotificationRepository {
+    public Notification createNotification(int Id, String desc, Timestamp time, int senderId, boolean isMessage, int chat_id);
+    public ArrayList<Notification>getAllNotification(int userId);
+    public void UpdateNotificationState(int NotificationId,int userId);
+
+}
